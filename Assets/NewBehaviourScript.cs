765850e558abb5eee847cddb791abb62e01802ed
xFIREx2005 <kaique.santos150@etec.sp.gov.br>
2021-05-17 16:57:06 -0300

    Arquivos da cena 2d

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+ 
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnCollisionEnter(Collision Colider) {
+        if (Colider.gameObject.tag == "Finish")
+        {
+            Application.LoadLevel(Application.loadedLevel);
+        }
+    }
+}
